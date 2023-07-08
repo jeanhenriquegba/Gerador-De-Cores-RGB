e07dfe957b0531e79e1d3d0f5fce6d019b1502f9
Jean Henrique <133189245+jeanhenriquegba@users.noreply.github.com>
2023-07-08 03:40:32 -0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+// Variaveis globais
+const btnGerar = document.querySelector(".trocarCor");
+const corExibida = document.querySelector(".corContainer");
+const cor = document.getElementById("cor");
+const corHistory = document.getElementById("corHistory");
+let corGuardada = 'Nenhuca cor foi computada ainda.';
+
+//Botão para gerar a cor
+btnGerar.addEventListener('click',()=>{
+
+let resultado = 'rgb(';
+for(let i = 0; i < 1 ;i++){resultado += Math.floor(Math.random()*254);};
+resultado = resultado +',';
+for(let i = 0; i < 1 ;i++){resultado += Math.floor(Math.random()*254);};
+resultado = resultado +',';
+for(let i = 0; i < 1 ;i++){resultado += Math.floor(Math.random()*254);};
+resultado = resultado +')';
+
+
+corExibida.textContent = resultado;
+document.body.style.backgroundColor = resultado;
+
+cor.style.color = resultado;
+corGuardada = resultado;
+
+corHistory.innerHTML += 
+`<span style="background:${resultado};">${resultado}</span>`;
+
+});
+
+// Botão para copiar a cor
+document.getElementById("cor").addEventListener('click',()=>{
+    if(corGuardada == ''){}else{
+    navigator.clipboard.writeText(corGuardada)
+    cor.style.color = '#00e600';
+    cor.innerHTML = 'Copiado &#10003;';
+    setTimeout(() => {
+    cor.style.color = corGuardada;
+    cor.textContent = 'Copiar cor para Ctrl+V';
+    }, 2000);
+    }
+});
